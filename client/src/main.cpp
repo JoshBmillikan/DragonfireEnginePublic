144e5017cb2b0f058ae7ab3b951bd21249676b85
josh <jmillikan12345@gmail.com>
2023-02-13 21:40:08 -0500

    update includes

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -2,7 +2,7 @@
 // Created by josh on 11/9/22.
 //
 #include "local_game.h"
-#include <SDL2/SDL_main.h>
+#include <SDL_main.h>
 
 // declared extern c for compatability with SDL_main
 extern "C" int main(int argc, char** argv)
